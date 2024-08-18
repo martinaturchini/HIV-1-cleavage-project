d34b8595a94a80439e15e686f3efee0bcbac589a
martinaturchini <93327748+martinaturchini@users.noreply.github.com>
2024-08-18 19:44:25 +0200

    Create plot_history.py

---
+++ source/plot_history.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+def plot_history(network_history, mod_num):
+ """
+    Plotting loss and accuracy values obtained for each epoch.
+ """
+ plt.figure()
+ plt.xlabel('Epochs')
+ plt.ylabel('Loss')
+ plt.plot(network_history.history['loss'], '--')
+ plt.plot(network_history.history['val_loss'], '-')
+ plt.legend(['Training', 'Validation'],
+              frameon=False)
+ plt.savefig(f"{file_dir}//HIV-CP Plots/Loss MLP "+mod_num+".png")
+
+ plt.figure()
+ plt.xlabel('Epochs')
+ plt.ylabel('Accuracy')
+ plt.plot(network_history.history['accuracy'], '--')
+ plt.plot(network_history.history['val_accuracy'], '-')
+ plt.legend(['Training', 'Validation'],
+             loc='lower right', frameon=False)
+ plt.savefig(f"{file_dir}//HIV-CP Plots/Accuracy MLP "+mod_num+".png")
+
+ plt.show()
