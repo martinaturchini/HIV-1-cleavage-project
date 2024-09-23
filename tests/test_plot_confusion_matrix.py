7b09d2c631093b5ce598ee60d3f551a7f287737c
martinaturchini <93327748+martinaturchini@users.noreply.github.com>
2024-09-23 19:22:55 +0200

    Update test_plot_confusion_matrix.py

--- tests/test_plot_confusion_matrix.py
+++ tests/test_plot_confusion_matrix.py
@@ -4,15 +4,10 @@ import shutil
 from source.plot_confusion_matrix import plot_confusion_matrix
 
 def test_plot_confusion_matrix():
-    # Create a sample confusion matrix
     cm = np.array([[50, 2, 1],
                    [10, 30, 5],
                    [2, 1, 35]])
-
-    # Define class names
     classes = ['Class A', 'Class B', 'Class C']
-
-    # Define method name
     method = 'Test Method'
 
     # Set the directory for saving the file
@@ -20,7 +15,6 @@ def test_plot_confusion_matrix():
     if not os.path.exists(file_dir):
         os.makedirs(f"{file_dir}//HIV-CP Plots")
     
-    # Call the function
     plot_confusion_matrix(file_dir=file_dir, cm=cm, classes=classes, method=method)
 
     # Check if the file was created
